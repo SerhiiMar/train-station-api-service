784bd714ca65e510ac5357a536c0261d411d1ac6
Serhii Marynin <serhii.marynin@gmail.com>
2024-06-11 16:21:43 +0300

    Implement user registration, managing

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/station/", include("station.urls", namespace="station")),
+    path("api/v1/user/", include("user.urls", namespace="user")),
 ]
